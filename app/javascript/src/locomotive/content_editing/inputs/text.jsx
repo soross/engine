c7891a97a2f8e73300a0d04e180d31eef3ec8301
Julien Girard <julien09girard@gmail.com>
2018-06-14 10:28:53 +0200

    test to show charles

--- app/javascript/src/locomotive/content_editing/inputs/text.jsx
+++ app/javascript/src/locomotive/content_editing/inputs/text.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
 
 class TextInput extends Component {
 
@@ -8,28 +9,64 @@ class TextInput extends Component {
     var value = props.data.settings[props.setting.id];
     if (value === undefined) value = props.setting.default || '';
 
-    this.state = { value };
+    var html = props.setting || false;
+
+    var rows = props.setting || 10;
+
+    var line_break = props.settings || false;
+
+    this.state = {
+      value,
+      html,
+      rows,
+      line_break
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {
-    const { value } = event.target;
-    this.setState({ value }, () => {
-      this.props.onChange(this.props.setting.type, this.props.setting.id, value)
-    });
+  handleChange(value) {
+    switch(this.state.html) {
+      case "true": {
+        this.setState({ value }, () => {
+          this.props.onChange(this.props.setting.type, this.props.setting.id, value)
+        });
+        break;
+      }
+      default: {
+        const { value } = value.target;
+        this.setState({ value }, () => {
+          this.props.onChange(this.props.setting.type, this.props.setting.id, value)
+        });
+        break;
+      }
+    }
   }
 
   render() {
     const { setting } = this.props;
+    console.log(this.state.html)
+    switch(this.state.html) {
 
-    return (
-      <div className="editor-input editor-input-text">
-        <label>{setting.label}</label>
-        <br/>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-      </div>
-    )
+      case "true": {
+        return (
+          <ReactQuill theme="snow"
+                      value={this.state.value}
+                      onChange={this.handleChange} />
+        )
+        break;
+      }
+      default: {
+        return (
+          <div className="editor-input editor-input-text">
+            <label>{setting.label}</label>
+            <br/>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </div>
+        )
+        break;
+      }
+    }
   }
 
 }
