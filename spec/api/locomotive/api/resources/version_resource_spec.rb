082513a99972c5a6b8b5aca1f1985e797c8bde72
soross <pgyh@hotmail.com>
2018-09-19 17:19:32 +0800

    Update version_resource_spec.rb

--- spec/api/locomotive/api/resources/version_resource_spec.rb
+++ spec/api/locomotive/api/resources/version_resource_spec.rb
@@ -4,12 +4,12 @@ describe Locomotive::API::Resources::VersionResource do
 
   include Rack::Test::Methods
 
-  describe 'GET /locomotive/api/v3/version.json' do
+  describe 'GET /jl/api/v3/version.json' do
     context 'JSON' do
       subject { parsed_response }
 
       it 'returns the current engine version' do
-        get '/locomotive/api/v3/version.json'
+        get '/jl/api/v3/version.json'
         expect(subject).to eq({ "engine" => Locomotive::VERSION })
       end
 
